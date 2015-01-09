28396e51118af6930991b490f5445a0041575883
Nick Hale <nicholas.hale@gmail.com>
2015-01-09 12:44:16 +0000

    Add retrieval of the days racecards

--- horses.py
+++ horses.py
@@ -1,18 +1,43 @@
-import bs4
+from bs4 import BeautifulSoup
+from datetime import date
 import re
 import requests
 
-# At the moment the index_url contains all the card information for the day. This may have to change.
-root_url = 'http://www.racingpost.com'
-index_url = root_url + '/horses2/cards/meeting_of_cards.sd?crs_id=10&r_date=2015-01-08&tab=sc_'
+def get_todays_horse_racing_cards():
+    """
+    Return a list of dictionaries containing the keys 'name' and 'link' for
+    the racecards of each of todays races.
+    """
+    index_url = 'http://www.racingpost.com/horses2/cards/home.sd?r_date=$DATE$'
+    index_url = index_url.replace('$DATE$', date.today().isoformat())
+    response = requests.get(index_url)
+    soup = BeautifulSoup(response.text)
+    # Get a list of all the meetings and the links to their racecards.
+    links = ([a.attrs.get('href') for a in soup.select('div.crBlock p.bull.show a')])
 
-def get_betting_forecast():
+    # Remove not standard racecard links:
+    #   ww - Worldwide Stakes Races
+    for link in links:
+        if link.endswith('ww'):
+            links.remove(link)
+
+    meetings = ([a.get_text() for a in soup.select('td.meeting h3 a')])
+
+    racecards = []
+    for meeting in meetings:
+        racecard = {}
+        racecard['name'] = meeting
+        racecard['link'] = links[meetings.index(meeting)]
+        racecards.append(racecard)
+
+    return racecards
+
+def get_betting_forecast(racecard):
     """Return the betting forecast as a list."""
-    # For testing purposes the following two lines are commented out. This is because the
-    # web page has been saved to disk.
-    #response = requests.get(index_url)
-    #soup = bs4.BeautifulSoup(response.text)
-    soup = bs4.BeautifulSoup(open('/home/nick/downloads/test.html'))                                        # Temporary
+    root_url = 'http://racingpost.com'
+    index_url = root_url + racecard['link']
+    response = requests.get(index_url)
+    soup = BeautifulSoup(response.text)
 
     # Return a list of all <p> tags that have an id beginning with "bettingForecastContainer".
     betting_forecast = [a.get_text() for a in soup.select('div.info >  p[id^="bettingForecastContainer"]')]
@@ -20,7 +45,7 @@ def get_betting_forecast():
     # The regex pattern matches against the following, "2/1 Horse Name,"
     pattern = re.compile('((\d+/\d+)\s(.+?)),')
 
-    # This will need changing. At the moment it is a hard-coded slection on which card to
+    # This will need changing. At the moment it is a hard-coded selection on which card to
     # find all the matches against.
     betting_forecast_dict = {}
 
@@ -30,6 +55,10 @@ def get_betting_forecast():
     return betting_forecast_dict
 
 if __name__ == '__main__':
-    betting_forecast_dict = get_betting_forecast()
+    # Get a list of todays racecards.
+    racecards = get_todays_horse_racing_cards()
+
+    betting_forecast_dict = get_betting_forecast(racecards[1])
     for name, odds in betting_forecast_dict.items():
         print(name, odds)
+
