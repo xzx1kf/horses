c9215cb0cb389ceba73e6ccd8fa26337d1980c0b
Nick Hale <nicholas.hale@gmail.com>
2015-01-12 16:16:25 +0000

    Add get_race_title()

--- horses.py
+++ horses.py
@@ -35,12 +35,8 @@ def get_todays_racecards():
 
     return racecards
 
-def get_betting_forecast(racecard):
+def get_betting_forecast(soup):
     """Return the betting forecast as a list."""
-    root_url = 'http://racingpost.com'
-    index_url = root_url + racecard['link']
-    response = requests.get(index_url)
-    soup = BeautifulSoup(response.text)
 
     # Return a list of all <p> tags that have an id beginning with "bettingForecastContainer".
     betting_forecast = [a.get_text() for a in soup.select('div.info >  p[id^="bettingForecastContainer"]')]
@@ -57,11 +53,37 @@ def get_betting_forecast(racecard):
         betting_forecast_dict[entry[1]] = entry[2]
     return betting_forecast_dict
 
+def get_race_title(soup):
+    """Return the race title"""
+    for a in soup.select('table.raceHead td.raceTitle strong.uppercase a'):
+        print(a.get_text())
+
+def parse(racecard):
+    root_url = 'http://racingpost.com'
+    index_url = root_url + racecard['link']
+    response = requests.get(index_url)
+    soup = BeautifulSoup(response.text)
+
+    racecards = soup.select('div.cardBlock.lightCards')
+
+    for racecard in racecards:
+        # Get race title
+        get_race_title(racecard)
+
+        # Get the betting forecast
+        betting_forecast_dict = get_betting_forecast(racecard)
+        for name, odds in betting_forecast_dict.items():
+            print(name, odds)
+
+        import sys
+        sys.exit(0)
+
 if __name__ == '__main__':
     # Get a list of todays racecards.
+    # These aren't racecards. This in a dictionary of meeting name,
+    # and a link to the full list of races.
     racecards = get_todays_racecards()
 
-    betting_forecast_dict = get_betting_forecast(racecards[0])
-    for name, odds in betting_forecast_dict.items():
-        print(name, odds)
-
+    # parse racecard
+    for racecard in racecards:
+        parse(racecard)
